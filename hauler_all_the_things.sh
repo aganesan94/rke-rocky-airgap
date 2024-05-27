485a81e858b2aa9f326d201de3b1366987b6f48c
Arun Ganesan <cloudtelsolutionsllc@gmail.com>
2024-05-27 11:18:47 +0530

    hauler_all_things with templates

--- hauler_all_the_things.sh
+++ hauler_all_the_things.sh
@@ -135,68 +135,71 @@ cat airgap_hauler.yaml
 info "cat airgap_hauler.yaml"
 cat airgap_hauler.yaml
 
-#
-#  curl -sL https://github.com/rancher/rancher/releases/download/$RANCHER_VERSION/rancher-images.txt -o hauler_temp/orig-rancher-images.txt
-#  sed -E '/neuvector|minio|gke|aks|eks|sriov|harvester|mirrored|longhorn|thanos|tekton|istio|hyper|jenkins|windows/d' hauler_temp/orig-rancher-images.txt >hauler_temp/cleaned-rancher-images.txt
-#
-#  # capi fixes
-#  grep cluster-api hauler_temp/orig-rancher-images.txt >>hauler_temp/cleaned-rancher-images.txt
-#  grep kubectl hauler_temp/orig-rancher-images.txt >>hauler_temp/cleaned-rancher-images.txt
-#
-#  # get latest version
-#  for i in $(cat hauler_temp/cleaned-rancher-images.txt | awk -F: '{print $1}'); do
-#    grep -w "$i" hauler_temp/cleaned-rancher-images.txt | sort -Vr | head -1 >>hauler_temp/rancher-unsorted.txt
-#  done
-#
-#  # final sort
-#  sort -u hauler_temp/rancher-unsorted.txt >hauler_temp/rancher-images.txt
-#
-#  # kubectl fix
-#  echo "rancher/kubectl:v1.20.2" >>hauler_temp/rancher-images.txt
-#
-#  for i in $(cat hauler_temp/rancher-images.txt); do echo "    - name: "$i >>airgap_hauler.yaml; done
-#
-#  rm -rf hauler_temp
-#
-#  cat <<EOF >>airgap_hauler.yaml
-#---
-#apiVersion: content.hauler.cattle.io/v1alpha1
-#kind: Charts
-#metadata:
-#  name: rancher-charts
-#spec:
-#  charts:
-#    - name: rancher
-#      repoURL: https://releases.rancher.com/server-charts/latest
-#      version: $RANCHER_VERSION
-#    - name: cert-manager
-#      repoURL: https://charts.jetstack.io
-#      version: $CERT_VERSION
-#    - name: longhorn
-#      repoURL: https://charts.longhorn.io
-#      version: $LONGHORN_VERSION
-#    - name: core
-#      repoURL: https://neuvector.github.io/neuvector-helm/
-#      version: $NEU_VERSION
-#---
-#apiVersion: content.hauler.cattle.io/v1alpha1
-#kind: Files
-#metadata:
-#  name: rancher-files
-#spec:
-#  files:
-#    - path: https://github.com/rancher/rke2-packaging/releases/download/v$RKE_VERSION%2Brke2r1.stable.0/rke2-common-$RKE_VERSION.rke2r1-0.$EL.x86_64.rpm
-#    - path: https://github.com/rancher/rke2-packaging/releases/download/v$RKE_VERSION%2Brke2r1.stable.0/rke2-agent-$RKE_VERSION.rke2r1-0.$EL.x86_64.rpm
-#    - path: https://github.com/rancher/rke2-packaging/releases/download/v$RKE_VERSION%2Brke2r1.stable.0/rke2-server-$RKE_VERSION.rke2r1-0.$EL.x86_64.rpm
-#    - path: https://github.com/rancher/rke2-selinux/releases/download/v0.17.stable.1/rke2-selinux-0.17-1.$EL.noarch.rpm
-#    - path: https://get.helm.sh/helm-$(curl -s https://api.github.com/repos/helm/helm/releases/latest | jq -r .tag_name)-linux-amd64.tar.gz
-#    - path: https://raw.githubusercontent.com/clemenko/rke_airgap_install/main/hauler_all_the_things.sh
-#  # - path: https://download.rockylinux.org/pub/rocky/9/isos/x86_64/Rocky-9.3-x86_64-dvd.iso
-#EOF
-#
-#  echo -n "  - created airgap_hauler.yaml"
-#  info_ok
-#
+
+  curl -sL https://github.com/rancher/rancher/releases/download/$RANCHER_VERSION/rancher-images.txt -o hauler_temp/orig-rancher-images.txt
+  sed -E '/neuvector|minio|gke|aks|eks|sriov|harvester|mirrored|longhorn|thanos|tekton|istio|hyper|jenkins|windows/d' hauler_temp/orig-rancher-images.txt >hauler_temp/cleaned-rancher-images.txt
+
+  # capi fixes
+  grep cluster-api hauler_temp/orig-rancher-images.txt >>hauler_temp/cleaned-rancher-images.txt
+  grep kubectl hauler_temp/orig-rancher-images.txt >>hauler_temp/cleaned-rancher-images.txt
+
+  # get latest version
+  for i in $(cat hauler_temp/cleaned-rancher-images.txt | awk -F: '{print $1}'); do
+    grep -w "$i" hauler_temp/cleaned-rancher-images.txt | sort -Vr | head -1 >>hauler_temp/rancher-unsorted.txt
+  done
+
+  # final sort
+  sort -u hauler_temp/rancher-unsorted.txt >hauler_temp/rancher-images.txt
+
+  # kubectl fix
+  echo "rancher/kubectl:v1.20.2" >>hauler_temp/rancher-images.txt
+
+  for i in $(cat hauler_temp/rancher-images.txt); do echo "    - name: "$i >>airgap_hauler.yaml; done
+
+  rm -rf hauler_temp
+
+  cat <<EOF >>airgap_hauler.yaml
+---
+apiVersion: content.hauler.cattle.io/v1alpha1
+kind: Charts
+metadata:
+  name: rancher-charts
+spec:
+  charts:
+    - name: rancher
+      repoURL: https://releases.rancher.com/server-charts/latest
+      version: $RANCHER_VERSION
+    - name: cert-manager
+      repoURL: https://charts.jetstack.io
+      version: $CERT_VERSION
+    - name: longhorn
+      repoURL: https://charts.longhorn.io
+      version: $LONGHORN_VERSION
+    - name: core
+      repoURL: https://neuvector.github.io/neuvector-helm/
+      version: $NEU_VERSION
+---
+apiVersion: content.hauler.cattle.io/v1alpha1
+kind: Files
+metadata:
+  name: rancher-files
+spec:
+  files:
+    - path: https://github.com/rancher/rke2-packaging/releases/download/v$RKE_VERSION%2Brke2r1.stable.0/rke2-common-$RKE_VERSION.rke2r1-0.$EL.x86_64.rpm
+    - path: https://github.com/rancher/rke2-packaging/releases/download/v$RKE_VERSION%2Brke2r1.stable.0/rke2-agent-$RKE_VERSION.rke2r1-0.$EL.x86_64.rpm
+    - path: https://github.com/rancher/rke2-packaging/releases/download/v$RKE_VERSION%2Brke2r1.stable.0/rke2-server-$RKE_VERSION.rke2r1-0.$EL.x86_64.rpm
+    - path: https://github.com/rancher/rke2-selinux/releases/download/v0.17.stable.1/rke2-selinux-0.17-1.$EL.noarch.rpm
+    - path: https://get.helm.sh/helm-$(curl -s https://api.github.com/repos/helm/helm/releases/latest | jq -r .tag_name)-linux-amd64.tar.gz
+    - path: https://raw.githubusercontent.com/clemenko/rke_airgap_install/main/hauler_all_the_things.sh
+  # - path: https://download.rockylinux.org/pub/rocky/9/isos/x86_64/Rocky-9.3-x86_64-dvd.iso
+EOF
+
+  echo -n "  - created airgap_hauler.yaml"
+  info_ok
+
+info "cat airgap_hauler.yaml"
+cat airgap_hauler.yaml
+
 #  warn "- hauler store sync - will take some time..."
 #  hauler store sync -f /opt/hauler/airgap_hauler.yaml || { fatal "hauler failed to sync - check airgap_hauler.yaml for errors"; }
 #  echo -n "  - synced"
